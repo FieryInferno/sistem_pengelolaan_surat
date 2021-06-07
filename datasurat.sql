e3b2d259aa70a7494b2ae24603bda76a5bcef07a
FieryInferno <bagassetia271@gmail.com>
2021-06-07 15:02:31 +0700

    penambahan notifikasi dan mengubah tanggal pada surat keluar

--- datasurat.sql
+++ datasurat.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 29 Mar 2021 pada 02.27
--- Versi server: 10.1.37-MariaDB
--- Versi PHP: 7.3.0
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jun 2021 pada 10.00
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,19 +31,22 @@ CREATE TABLE `data_user` (
   `id_user` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` text NOT NULL,
-  `level` enum('admin','kepala_tu','pengirim','penerima') NOT NULL,
+  `level` enum('admin','kepala_p3d','kepala_seksi','staff') NOT NULL,
   `nik` varchar(191) NOT NULL,
   `nama` varchar(191) NOT NULL,
-  `email` varchar(191) NOT NULL
+  `email` varchar(191) NOT NULL,
+  `seksi` varchar(191) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `data_user`
 --
 
-INSERT INTO `data_user` (`id_user`, `username`, `password`, `level`, `nik`, `nama`, `email`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '', '', ''),
-(2, 'asdfg', 'asdfg', 'pengirim', '123456789', 'asdfg', 'adminsimpeg@gmail.com');
+INSERT INTO `data_user` (`id_user`, `username`, `password`, `level`, `nik`, `nama`, `email`, `seksi`) VALUES
+(1, 'admin', 'admin', 'admin', '', '', '', NULL),
+(2, 'kepala_p3d', 'kepala_p3d', 'kepala_p3d', '123456789', 'asdfg', 'adminsimpeg@gmail.com', NULL),
+(3, 'kepala_seksi', 'kepala_seksi', 'kepala_seksi', '123456789', 'kepala tu', 'kepalatu@gmail.com', '1'),
+(4, 'indriya', 'indriya', 'staff', '2141434', 'Indriya', 'indriya@gmail.com', '1');
 
 -- --------------------------------------------------------
 
@@ -53,18 +55,23 @@ INSERT INTO `data_user` (`id_user`, `username`, `password`, `level`, `nik`, `nam
 --
 
 CREATE TABLE `surat_keluar` (
-  `Nomor_Surat` int(10) NOT NULL,
-  `Tanggal_Dikirim` date NOT NULL,
-  `Surat_Dikirim_Kepada` varchar(20) NOT NULL,
-  `Perihal` varchar(30) NOT NULL
+  `id_surat_keluar` int(11) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `perihal` varchar(191) NOT NULL,
+  `tujuan` varchar(191) NOT NULL,
+  `file` varchar(191) DEFAULT NULL,
+  `isi` text DEFAULT NULL,
+  `no_surat` varchar(191) DEFAULT NULL,
+  `status` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `surat_keluar`
 --
 
-INSERT INTO `surat_keluar` (`Nomor_Surat`, `Tanggal_Dikirim`, `Surat_Dikirim_Kepada`, `Perihal`) VALUES
-(123, '2021-03-17', 'ASN P3DW', 'Surat Tugas');
+INSERT INTO `surat_keluar` (`id_surat_keluar`, `tanggal`, `perihal`, `tujuan`, `file`, `isi`, `no_surat`, `status`) VALUES
+(6, '2021-04-13', 'a', 'afdf', '6085893bcf568.pdf', '<p>asdfghjkl</p>\r\n', '800/001-tu', '1'),
+(8, NULL, 'a', 'afdf', NULL, NULL, NULL, '0');
 
 -- --------------------------------------------------------
 
@@ -73,19 +80,28 @@ INSERT INTO `surat_keluar` (`Nomor_Surat`, `Tanggal_Dikirim`, `Surat_Dikirim_Kep
 --
 
 CREATE TABLE `surat_masuk` (
-  `Nomor_Surat` int(10) NOT NULL,
-  `Tanggal_Surat` date NOT NULL,
-  `Surat_Dari` varchar(20) NOT NULL,
-  `Tanggal_Diterima` date NOT NULL,
-  `Perihal` varchar(30) NOT NULL
+  `id_surat_masuk` int(11) NOT NULL,
+  `no_surat` varchar(191) NOT NULL,
+  `tanggal` date NOT NULL,
+  `perihal` varchar(191) NOT NULL,
+  `file` varchar(191) NOT NULL,
+  `pengirim` varchar(191) NOT NULL,
+  `seksi` int(191) DEFAULT NULL,
+  `status` int(191) NOT NULL DEFAULT 0,
+  `staff` int(191) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `surat_masuk`
 --
 
-INSERT INTO `surat_masuk` (`Nomor_Surat`, `Tanggal_Surat`, `Surat_Dari`, `Tanggal_Diterima`, `Perihal`) VALUES
-(128, '2021-03-14', 'Polsub', '2021-03-14', 'PKL');
+INSERT INTO `surat_masuk` (`id_surat_masuk`, `no_surat`, `tanggal`, `perihal`, `file`, `pengirim`, `seksi`, `status`, `staff`) VALUES
+(1, '123456', '2021-03-29', '-', '123456.pdf', '2', 1, 3, 4),
+(2, '123456', '2021-03-30', 'asdfg', '', '2', 1, 3, 4),
+(3, '145', '2021-03-30', 'asd', 'Surat_Lamaran_PT_TKII_M_Bagas_Setia.pdf', '2', NULL, 0, NULL),
+(4, '123456', '2021-03-30', 'asdfgh', 'Surat_Lamaran_PT_TKII_M_Bagas_Setia1.pdf', '2', NULL, 0, NULL),
+(5, '1', '2021-04-26', 'a', 'document1.pdf', 'a', NULL, 0, NULL),
+(6, '800/001-tu', '2021-06-07', 'a', 'document3.pdf', 'a', NULL, 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -101,13 +117,13 @@ ALTER TABLE `data_user`
 -- Indeks untuk tabel `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  ADD PRIMARY KEY (`Nomor_Surat`);
+  ADD PRIMARY KEY (`id_surat_keluar`);
 
 --
 -- Indeks untuk tabel `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  ADD PRIMARY KEY (`Nomor_Surat`);
+  ADD PRIMARY KEY (`id_surat_masuk`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -117,19 +133,19 @@ ALTER TABLE `surat_masuk`
 -- AUTO_INCREMENT untuk tabel `data_user`
 --
 ALTER TABLE `data_user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  MODIFY `Nomor_Surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `id_surat_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  MODIFY `Nomor_Surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
+  MODIFY `id_surat_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
